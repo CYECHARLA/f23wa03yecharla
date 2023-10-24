2bb2e62dd5ad4c8726f7ed96895af08a04a65f03
CYECHARLA <142927564+CYECHARLA@users.noreply.github.com>
2023-10-24 16:41:42 -0500

    changes done on user.js file

--- routes/computation.js
+++ routes/computation.js
@@ -1,14 +1,35 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
-// ... (existing code)
-
-/* GET computation result. */
-router.get('/computation', function(req, res, next) {
-  const randomValue = req.query.x ? parseFloat(req.query.x) : Math.random() * 10; // Generate a random value between 0 and 10 or use the provided query value
-  const result = Math.atan2(randomValue, randomValue); // Apply Math.atan2() function
-
-  res.send(`Math.atan2() applied to ${randomValue} is ${result}`);
+router.get("/", function (req, res, next) {
+ var randNumArr = Math.random() * 2;
+ var rndNumArr = Math.round(randNumArr);
+ if (req.query.x) {
+  let x = req.query.x;
+  var arr = [Math.atan2(x), Math.atanh(x), Math.cbrt(x)];
+  var funObj = {
+   0: "Math.atan2()",
+   1: "Math.atanh()",
+   2: "Math.cbrt()",
+  };
+  var arr = [Math.atan32(x), Math.atanh(x), Math.cbrt(x)];
+  let response1 = `
+  Function ${funObj[rndNumArr]} applied to x: ${x} the result is ${arr[rndNumArr].toFixed(3)}`;
+  res.send(response1);
+ }
+ else {
+  var randNumArr = Math.random() * 2;
+  var rndNumArr = Math.round(randNumArr);
+  var randNum = Math.random() * 10;
+  var x = Math.round(randNum);
+  var funObj = {
+   0: "Math.atan2()",
+   1: "Math.atanh()",
+   2: "Math.cbrt()",
+  };
+  var arr = [Math.atan(x), Math.exp(x), Math.expm1(x)];
+  let response = `
+  Function ${funObj[rndNumArr]} applied to the random value for x from 1 to 10 is: ${x} and the result is ${arr[rndNumArr].toFixed(3)}`
+  res.send(response);
+ }
 });
-
 module.exports = router;
